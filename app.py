31b694f3a4d88c1a06c2188f1dd0f19378badd85
jftigre <joaofelipetigre@gmail.com>
2026-02-14 02:01:09 -0300

    refatora a inicialização do app Flask para a função create_app

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, jsonify, request
+from flask import Flask, app, jsonify, request
 from database import db
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://admin:admin123@127.0.0.1:3306/daily_diet"
 
-db.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "your_secret_key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://admin:admin123@127.0.0.1:3306/daily_diet"
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+    db.init_app(app)
+    db.create_all()
+    app.run()
+
 
 if __name__ == "__main__":
     with app.app_context():
