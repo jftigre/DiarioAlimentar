d75d2bcf4d5cb0c8cc08daafc195e69fe2dc07d7
jftigre <joaofelipetigre@gmail.com>
2026-02-14 00:05:14 -0300

    adiciona rota de teste e configurações do banco de dados no app

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from database import db
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] =
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://admin:admin123@127.0.0.1:3306/daily_diet"
 
+db.init_app(app)
 
-if __name__ == '__main__':
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
