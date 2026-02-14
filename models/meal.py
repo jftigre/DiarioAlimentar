8be1fd2b382a30e91bca2ef17abecf9de2f77f08
jftigre <joaofelipetigre@gmail.com>
2026-02-14 02:37:58 -0300

    corrige o modelo Meal para definir corretamente os tipos de coluna e adiciona valor padrão para a data

--- models/meal.py
+++ models/meal.py
@@ -1,17 +1,18 @@
 from database import db
+from datetime import datetime
 
 class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(200), nullable=False)
-    datetime = db.Column(db.DateTime, nullable=False)
-    is_diet = db.Column(db.DateTime, nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    is_diet = db.Column(db.Boolean, nullable=False)
 
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
             'description': self.description,
-            'datetime': self.datetime.isoformat(),
+            'datetime': self.datetime.isoformat() if self.datetime else None,
             'is_diet': self.is_diet 
         }
